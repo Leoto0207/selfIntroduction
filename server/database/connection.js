5e9cf606cc9b84e1bec27da1a1a6ec2209fc061e
LeoTo <leoto0207@gmail.com>
2023-10-02 17:02:02 +0800

    fix bug

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = async() => {
-try{
+const connectDB = async () => {
+  try {
     const db = await mongoose.connect(process.env.MONGO_URI);
     console.log(`The Server has connected:  ${db.connection.host}`);
-}catch(e){
-    console.log("Error", e);
+  } catch (e) {
+    console.log(e);
     process.exit(1);
-    }
-}
+  }
+};
 
 module.exports = connectDB;
