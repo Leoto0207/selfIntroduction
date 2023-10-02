fef291b626c637cee075477a3e342d429cc30d35
LeoTo <leoto0207@gmail.com>
2023-10-02 23:27:05 +0800

    update for deploy

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const connectdb = require("./server/database/connection");
 const Contactdb = require("./model/model");
+const router = express.Router();
+const serverless = require("serverless-http");
 
 dotenv.config({ path: "config.env" });
 
@@ -11,26 +13,32 @@ connectdb();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.set("views", path.join(__dirname, "/views"));
-app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  // res.render("index");
 });
 
-app.post("/add_contact", async (req, res) => {
+router.post("/add_contact", async (req, res) => {
   try {
     const { name, company, email, contact } = req.body;
     const newContact = new Contactdb({ name, company, email, contact });
     await newContact.save();
-    res.render("index");
+    res.sendFile(path.join(__dirname, "index.html"));
   } catch (e) {
     console.log(e);
     res.redirect("/");
   }
 });
 
+app.use("/", router);
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`the app started at port ${port}`);
 });
+
+// app.use("/.netlify/functions/api", router);
+// module.exports.handler = serverless(app);
--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,34 @@
-const faders = document.querySelectorAll('.fading');
-const sliders = document.querySelectorAll('.slide-in');
-
+const faders = document.querySelectorAll(".fading");
+const sliders = document.querySelectorAll(".slide-in");
 
 const sectionOneOptions = {
-    rootMargin: '-200px 0 px 0px 0px'
+  rootMargin: "-200px 0 px 0px 0px",
 };
 
 const appearOptions = {
-    threshold: 0,
-    rootMargin: '0px 0px -200px 0px'
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
 };
 
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries, appearOnScroll
-    ){
-        entries.forEach(entry=> {
-            if(!entry.isIntersecting){
-                return;
-            }else{
-                entry.target.classList.add('appear');
-                appearOnScroll.unobserve(entry.target);
-            }
-        })
-}, appearOptions);
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
 });
 
-sliders.forEach(slider=>{
-    appearOnScroll.observe(slider);
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
-
-
-
